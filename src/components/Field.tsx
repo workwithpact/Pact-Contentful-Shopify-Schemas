9405e3c5699072b9ce9bd9971c8d61148be19e8b
Alex Leclair <alex@213a.ca>
2022-05-12 10:50:50 -0400

    Added new types: Header, Paragraph, Checkbox, Number

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,14 +1,26 @@
-import { TextInput, Select, Textarea } from "@contentful/f36-components";
-import React, { FormEvent } from "react";
+import { TextInput, Select, Textarea, Subheading, Text, Checkbox } from "@contentful/f36-components";
+import React from "react";
 
 const Field = ({ setting, value, onChange }: FieldProps) => {
-  console.log({setting})
   const handleOnChange = (event:any) => onChange && event?.target ?  onChange((event.target as any).value) : null;
   switch (setting.type) {
+    case "header": 
+      return setting.content ? <Subheading>{setting.content}</Subheading> : null;
+    case "paragraph": 
+      return setting.content ? <Text>{setting.content}</Text> : null;
     case "text":
       return <TextInput value={value} onChange={handleOnChange} />;
+    case "checkbox":
+      return <Checkbox
+        isChecked={value}
+        onChange={handleOnChange}
+      >
+        {setting.label}
+      </Checkbox>
     case "textarea":
       return <Textarea value={value} onChange={handleOnChange} />;
+    case "number":
+      return <TextInput type="number" min={setting.min || null} max={setting.max || null} value={value} onChange={handleOnChange} />;
     case "select":
       return (
         <Select
@@ -26,7 +38,7 @@ const Field = ({ setting, value, onChange }: FieldProps) => {
     // case "boolean":
       // return null
   }
-  return <>Unknown field type</>
+  return <>Unknown field type: {setting?.type}</>
 }
 
 export interface FieldProps {
