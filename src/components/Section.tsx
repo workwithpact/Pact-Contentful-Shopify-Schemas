8e75bf1f03301ffe9fbd88db2f41374b05e3d617
Alex Leclair <alex@213a.ca>
2022-05-16 07:48:49 -0400

    Expand sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useFieldValue } from '@contentful/react-apps-toolkit';
-import { Button, Card, Heading, Menu } from "@contentful/f36-components";
+import { Button, Card, Collapse, Flex, Heading, Menu } from "@contentful/f36-components";
 import Settings from "./Settings";
 import BlocksList from "./BlocksList";
 
@@ -15,50 +15,55 @@ const Section = ({ config, field }: SectionProps) => {
       margin:'1rem auto',
     }}>
       <Card>
-        <Heading>{config?.config?.name || 'Section Settings'}</Heading>
-        <Settings settings={settings} value={value} setValue={setValue} config={config} />
+        <Flex>
+          <Heading>{config?.config?.name || 'Section Settings'}</Heading>
+          <Button size="small" style={{margin: "auto 0 auto auto"}} onClick={() => setIsExpanded(!isExpanded)}>{isExpanded ? 'Collapse' : 'Expand'}</Button>
+        </Flex>
+        <Collapse isExpanded={isExpanded}>
+          <Settings settings={settings} value={value} setValue={setValue} config={config} />
 
-        {
-          !blocks.length ? null : (
-            <>
-              <hr />
-              <Menu>
-                <Menu.Trigger>
-                  <Button>Add block</Button>
-                </Menu.Trigger>
-                <Menu.List>
-                  {
-                    blocks.map((block: any) => {
-                      return (
-                        <Menu.Item key={`${config.field}|${block.id}`} onClick={() => {
-                            setValue({
-                              ...(value || {}),
-                              blocks: [
-                                ...(value?.blocks || []),
-                                {
-                                  id: block.id,
-                                  type: block.type,
-                                  settings: {}
-                                }
-                              ]
-                            })
-                          }}>{block.name}
-                        </Menu.Item>
-                      )})
-                  }
-                </Menu.List>
-              </Menu>
-            </>
-          )
-        }
-        <BlocksList 
-          blocks={value?.blocks || []} 
-          config={config} 
-          onChange={(newBlocks) => setValue({
-            ...(value || {}),
-            blocks: [...newBlocks]
-          })} />
-      </Card>
+          {
+            !blocks.length ? null : (
+              <>
+                <hr />
+                <Menu>
+                  <Menu.Trigger>
+                    <Button>Add block</Button>
+                  </Menu.Trigger>
+                  <Menu.List>
+                    {
+                      blocks.map((block: any) => {
+                        return (
+                          <Menu.Item key={`${config.field}|${block.id}`} onClick={() => {
+                              setValue({
+                                ...(value || {}),
+                                blocks: [
+                                  ...(value?.blocks || []),
+                                  {
+                                    id: block.id,
+                                    type: block.type,
+                                    settings: {}
+                                  }
+                                ]
+                              })
+                            }}>{block.name}
+                          </Menu.Item>
+                        )})
+                    }
+                  </Menu.List>
+                </Menu>
+              </>
+            )
+          }
+          <BlocksList 
+            blocks={value?.blocks || []} 
+            config={config} 
+            onChange={(newBlocks) => setValue({
+              ...(value || {}),
+              blocks: [...newBlocks]
+            })} />
+          </Collapse>
+        </Card>
     </div>
   )
 };
