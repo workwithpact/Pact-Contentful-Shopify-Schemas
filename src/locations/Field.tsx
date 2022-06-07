eb5eefb9a2f4384853af481aec131f3c46327442
Alex Leclair <alex@213a.ca>
2022-06-06 21:27:13 -0400

    Remove unreachable code

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -70,15 +70,6 @@ const Field = () => {
     <Spinner />
   </Flex>
   );
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
 };
 
 export default Field;
