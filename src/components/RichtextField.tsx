a3cfa92131889b895fb7050441b9ae46c3273249
Alex Leclair <alex@213a.ca>
2022-06-06 21:26:27 -0400

    Rich Text support

--- src/components/RichtextField.tsx
+++ src/components/RichtextField.tsx
@@ -1,13 +1,53 @@
-import { Textarea } from "@contentful/f36-components";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 import React from "react";
 
-const RichtextField = ({value, onChange}: RichtextFieldProps) => {
-  return <Textarea value={value} onChange={onChange} />; // Todo: Implement WYSIWYG
-}
-
-export interface RichtextFieldProps {
-  value: any;
+class RichtextField extends React.Component<RichtextFieldProps, RichtextFieldState> {
   onChange: (newValue: any) => void;
+  state: {
+    value: string;
+  }
+  constructor(props: RichtextFieldProps) {
+    const {value, onChange} = props;
+    super(props);
+    this.onChange = onChange;
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: value || '',
+    };
+  }
+
+  handleChange = (content: string) => {
+    this.setState({
+        value: content
+    })
+    this.onChange({
+      target: {
+        value: content,
+        type: 'Richtext'
+      }
+    });
+ };
+
+  render() {
+    return (
+      <ReactQuill
+        value={this.state.value}
+        onChange={this.handleChange}
+        theme="snow"
+        />
+    );
+  }
 }
 
 export default RichtextField;
+
+export interface RichtextFieldProps {
+  value: string;
+  onChange: (newValue: any) => void;
+}
+
+export interface RichtextFieldState {
+  value: string;
+}
